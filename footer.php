c0129980753c6c14bc68a409dcd3f0447c85d91f
karolinaskorupska <pixie22@op.pl>
2021-02-17 21:48:49 +0100

    html body, header, main, footer

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
       <div class="footer">
           <footer>
-              <h3>FOOTER</h3>
+              <div class="footer__photo-container">
+                    <div class="footer__photo-container--photo">
+                        <!-- <img scr="<?php echo get_template_directory_uri(); ?>" alt="photo"> -->
+                    </div>
+                    <div class="footer__photo-container--logo">
+                        <img scr="<?php echo get_template_directory_uri(); ?>assets/images/icons/minimum_logo.png" alt="logo Minimum Studio">
+                    </div>
+              </div>
+              <div class="footer__contact-container">
+                    <ul>
+                        <li>wysoka 12/17</li>
+                        <li>poznań</li>
+                        <li>poland</li>
+                        <li>hello@minimumstudio.pl</li>
+                        <li><a href="#">projekty</a></li>
+                    
+                    </ul>
+              </div>  
           </footer>
       </div>
     </div>
